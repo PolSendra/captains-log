2158d3ac3c2451c37a62dd45437631368e9f9e09
psendra <psendra@absis.es>
2016-09-07 11:15:58 +0200

    unit tests for the log functions

--- tests/source/logFunction.test.js
+++ tests/source/logFunction.test.js
@@ -1,31 +1,70 @@
 /* eslint-env mocha */
 import { expect } from 'chai';
 import sinon from 'sinon';
-import log from '../../source/logger.js';
-import { logConsoleWithLog, logConsoleWithError, logFileWithLog, logFileWithError } from '../../source/logFunction.js';
+import * as writeInterface from '../../source/interfaces/writeFileInterface';
+import { logConsoleWithLog,
+            logConsoleWithError,
+            logFileWithLog,
+            logFileWithError
+        } from '../../source/logFunction.js';
 
 describe('Test the log functions', () => {
-    describe('logConsoleWithLog', () => {
-        it('should logPath is undefined', () => {
-
+    describe('Tests the console writeing log funcitons', () => {
+        describe('logConsoleWithLog', () => {
+            it('should logPath is undefined and message', () => {
+                let result = logConsoleWithLog(undefined, 'missatge');
+                expect(result).to.be.true;
+            });
+            it('should logPath is defined and message', () => {
+                let result = logConsoleWithLog('some path', 'missatge');
+                expect(result).to.be.true;
+            });
         });
-        it('', () => {
-
-        });
-        it('', () => {
-
-        });
-        it('', () => {
-
+        describe('logConsoleWithError', () => {
+            let error = new Error('some test error');
+            it('should logPath is undefined and Error message', () => {
+                let result = logConsoleWithError(undefined, error);
+                expect(result).to.be.true;
+            });
+            it('should logPath is defined and Error message', () => {
+                let result = logConsoleWithError('some path', error);
+                expect(result).to.be.true;
+            });
         });
     });
-    describe('logConsoleWithError', () => {
+    describe('Tests the file writeing log funcitons', () => {
+        let stubWriteFile;
+        beforeEach(() => {
+            stubWriteFile = sinon.stub(writeInterface, 'writeLog').returns(true);
+        });
+        afterEach(() => {
+            stubWriteFile.restore();
+        });
+        describe('logFileWithLog', () => {
+            it('should return true with logPath is undefined and string message', () => {
+                let result = logFileWithLog(undefined, 'some error text');
+                expect(result).to.be.true;
+                expect(stubWriteFile.calledOnce).to.be.true;
+            });
+            it('should return true with logPath is defined and string message', () => {
+                let result = logFileWithLog('some path', 'some error text');
+                expect(result).to.be.true;
+                expect(stubWriteFile.calledOnce).to.be.true;
+            });
+        });
+        describe('logFileWithError', () => {
+            let error = new Error('some test error');
 
-    });
-    describe('logFileWithLog', () => {
-
-    });
-    describe('logFileWithError', () => {
-
+            it('should return true with logPath is undefined and Error message', () => {
+                let result = logFileWithError(undefined, error);
+                expect(result).to.be.true;
+                expect(stubWriteFile.calledOnce).to.be.true;
+            });
+            it('should return true with logPath is defined and message', () => {
+                let result = logFileWithError('some path', error);
+                expect(result).to.be.true;
+                expect(stubWriteFile.calledOnce).to.be.true;
+            });
+        });
     });
 });
