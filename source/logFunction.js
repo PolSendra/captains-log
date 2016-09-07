666bca697833eac59eb482467c65aa66db84f954
psendra <psendra@absis.es>
2016-09-07 11:16:36 +0200

    added function for the wrieting in the file cases

--- source/logFunction.js
+++ source/logFunction.js
@@ -1,52 +1,56 @@
-//  write the date in the console
+import { writeLog } from './interfaces/writeFileInterface';
 
 function logConsoleWithLog (logPath, logMessage) {
-    //    console.log('logPath => ' + logPath);
-    //    console.log('logMessage => ' + logMessage);
     if (logPath === undefined) {
         console.log(
-            `Log message: ${logMessage}`
+            `[LOG] : Log message: ${logMessage}`
         );
     } else {
         console.log(
-            `Log in file ${logPath}.
+            `[LOG] : Log in file ${logPath}.
              with message: ${logMessage}`
         );
     }
+    return true;
 }
 
 function logConsoleWithError (logPath, logMessage) {
     //
     if (logPath === undefined) {
         console.error(
-            `Error with message: ${logMessage.message}.
-            Error with trace ${logMessage.stack}.`
+            `[ERROR] : ${logMessage.stack}.`
         );
     } else {
         console.error(
-            `Error in file ${logPath}.
-            Error with message: ${logMessage.message}.
-            Error with trace ${logMessage.stack}.`
+            `[ERROR] : Error in file ${logPath}.
+            ${logMessage.stack}.`
         );
     }
+    return true;
 }
 
-function logFileWithLog (logType, logPath, logMessage) {
+function logFileWithLog (logPath, logMessage) {
     //
-    if (logPath === 'undefined') {
-        
+    let result;
+    const date = new Date().toLocaleString();
+    if (logPath === undefined) {
+        result = writeLog(`[LOG][${date}] : (${logMessage})`);
     } else {
-
+        result = writeLog(`[LOG][${date}][${logPath}] : (${logMessage})`);
     }
+    return result;
 }
 
-function logFileWithError (logType, logPath, logMessage) {
+function logFileWithError (logPath, logMessage) {
     //
-    /* if (logPath === 'undefined') {
-
+    let result;
+    const date = new Date().toLocaleString();
+    if (logPath === undefined) {
+        result = writeLog(`[ERROR][${date}] : (${logMessage.message}) | [Trace] : (${logMessage.stack})`);
     } else {
-
-    }*/
+        result = writeLog(`[ERROR][${date}][${logPath}] : (${logMessage.message}) | [Trace] : (${logMessage.stack})`);
+    }
+    return result;
 }
 
 export {logConsoleWithLog, logConsoleWithError, logFileWithLog, logFileWithError};
