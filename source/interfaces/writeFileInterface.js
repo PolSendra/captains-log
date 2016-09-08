ece6740c9a0770f6f40a0ce3cffb52f2eecb9a10
PolSendra <pol.sendra@est.fib.upc.edu>
2016-09-08 21:49:10 +0200

    write file interface cleaned

--- source/interfaces/writeFileInterface.js
+++ source/interfaces/writeFileInterface.js
@@ -2,14 +2,11 @@ import fs from 'fs';
 
 function writeLog (logMessage) {
     //  put the date on the message log
-    console.log('entro writelog message ' + logMessage);
     fs.appendFile('logsFile.log', logMessage + '\r\n', (err) => {
         if (err) {
-            console.log('retorno error');
             throw err;
         }
     });
-    return true;
 }
 
 export {writeLog};
