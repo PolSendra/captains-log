21532272a6f21651b23e055669df12052bc355ba
PolSendra <pol.sendra@est.fib.upc.edu>
2016-09-08 21:47:59 +0200

    logger main cleaned

--- source/logger.js
+++ source/logger.js
@@ -1,16 +1,10 @@
 import { logConsoleWithLog, logConsoleWithError, logFileWithLog, logFileWithError } from './logFunction';
-/**
- * [log description]
- * @param  {[type]} logOutput  [description]
- * @param  {[type]} logType    [type of log => verbose/error/]
- * @param  {[type]} logPath    [description]
- * @param  {[type]} logMessage [description]
- * @return {[type]}            [description]
- */
+
 export default (logMessage, logOutput, logType, logPath) => {
     //  see what env we are on
     const env = process.env.NODE_ENV;
     //    console.log('environment => ' + env);
+
     //    logOutput
     if (logOutput === undefined) {
         if (env === 'production' || env === 'test') {
@@ -26,6 +20,7 @@ export default (logMessage, logOutput, logType, logPath) => {
 
     //  we force logOutput to file in environment eql to Production
     if (env === 'production' || env === 'test') logOutput = 'file';
+
     //  logType
     if (logType === undefined) {
         if (logMessage instanceof Error) {
@@ -36,13 +31,13 @@ export default (logMessage, logOutput, logType, logPath) => {
     } else if (logType === 'log' || logType === 'l') {
         logType = 'log';
     } else logType = 'log'; //    maybe put error here
+
     //  message
     if (logMessage instanceof Error) {
         logType = 'error';
     } else logType = 'log';
     if (logMessage === undefined) {
         // let error = new Error('error passing the message to de log');
-        // log(error);
     }
 
     //  console.log('logtype => ' + logType);
