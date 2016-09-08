53a1a69b0d715ad9d59990e949dd9cdda993b238
PolSendra <pol.sendra@est.fib.upc.edu>
2016-09-08 21:21:01 +0200

    file interface tests need more thinking

--- source/interfaces/writeFileInterface.js
+++ source/interfaces/writeFileInterface.js
@@ -2,13 +2,14 @@ import fs from 'fs';
 
 function writeLog (logMessage) {
     //  put the date on the message log
-    console.log('entro writelog');
-    fs.appendFile('logsFile.log', logMessage + '\r\n', (err) => {
+    console.log('entro writelog message ' + logMessage);
+    fs.appendFileS('logsFile.log', logMessage + '\r\n', (err) => {
         if (err) {
             console.log('retorno error');
             throw err;
         }
     });
+    return true;
 }
 
 export {writeLog};
--- tests/source/interfaces/writeFileInterface.test.js
+++ tests/source/interfaces/writeFileInterface.test.js
@@ -1,22 +1,22 @@
 /* eslint-env mocha */
-import { expect } from 'chai';
+//  import { expect } from 'chai';
 import sinon from 'sinon';
 import { writeLog } from '../../../source/interfaces/writeFileInterface';
 import * as fs from 'fs';
 
-let stub;
 describe('Test the writeing interface', () => {
-    beforeEach(() => {
-
-    });
-    afterEach(() => {
+    it('should return true', () => {
+        let stub = sinon.stub(fs, 'appendFileSync').returns(false);
+        writeLog('some message1');
+        //  expect(stub.calledOnce).to.be.true;
+        //  expect(result).to.be.true;
         stub.restore();
     });
-    it('should return true', () => {
-        stub = stub = sinon.stub(fs, 'appendFile').returns(true);
-        writeLog('some message1');
-        //  console.log('result ' + result);
-        //  expect(stub.called).to.be.true;
-        //  stub.restore();
+    it('should throw error', () => {
+        let error = new Error('some error with fs');
+        let stub = sinon.stub(fs, 'appendFileSync').returns(error);
+        writeLog();
+        //  expect(result).to.be.true;
+        stub.restore();
     });
 });
